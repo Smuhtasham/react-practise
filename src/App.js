1ce98e3a562b8cf87a9e473065e40db23435607b
Syed Muhammad Muhtasham <muhtasham00@gmail.com>
2024-08-07 16:06:44 +0500

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import { useEffect, useState } from "react";
 
 function App() {
+  
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const intervel = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 1000);
+    return () => {
+      clearInterval(intervel);
+    };
+  }, []);
+
   return (
     <>
-    <div className="text-blue-600">Hi i am Syed</div>
+      <div className="text-blue-600">{count}</div>
     </>
   );
 }
