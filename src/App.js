90dce6fa8bfa3b384b5f78ae79a1b6e427f87e3a
Syed Muhammad Muhtasham <muhtasham00@gmail.com>
2024-08-09 15:47:24 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const url = "https://randomuser.me/api?results=50";
+  const [small, setSmall] = useState(false);
+  const [day, setDay] = useState(true);
+  const [stars, setStars] = useState(false);
 
   useEffect(() => {
-    const getData = async () => {
-      setLoading(true);
-      const res = await fetch(url);
-      const data = await res.json();
-      setCount(data);
-      setLoading(false);
-    };
-    getData();
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", () => {
+        setDay(window.scrollY > 0 && window.scrollY < 150);
+        setSmall(window.scrollY > 150);
+        setStars(window.scrollY > 260);
+      });
+      return () => {
+        setDay(false);
+      };
+    }
   }, []);
-  console.log(count)
-  return (
+  console.log(small);
+  return (  
     <>
-      <div className="">
-        <div className=" text-black">
-          <div>
-            {loading ? (
-              <p>Loading...</p>
-            ) : (
-              count?.results?.map((data,index) => {
-                return <div key={index} className="flex gap-6">
-                  <div>{data.name.first}</div>
-                  <div>{data.email}</div>
-                  </div>
-              })
-            )}
-          </div>
+      <div className={`h-[500px] bg-black`}>
+        <div className={`${day ? "" : "hidden"}`}>
+          <img src="pic0.jpg" alt="" />
         </div>
+        <div className={`transition translate-y-6 ease-in-out delay-1000 ${small ? "absolute " : "hidden"}`}>
+          <img className="h-[750px]" src="pic1.jpg" alt="" />
+        </div>
+        <div className={` ${stars ? "absolute top-[30%] " : "hidden"}`}>
+            <img className="w-[450px]" src="stars.gif" alt="" />
+          </div>
+        <div className={` ${stars ? "absolute left-[60%] top-[30%]" : "hidden"}`}>
+            <img className="w-[450px]" src="stars.gif" alt="" />
+          </div>
       </div>
     </>
   );
