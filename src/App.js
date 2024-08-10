9b88d4827e450bce470e58aeb2dfb7989e3fcb0f
Syed Muhammad Muhtasham <muhtasham00@gmail.com>
2024-08-10 23:11:23 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [scroll, setScroll] = useState("");
+  const [scroll, setScroll] = useState(0);
+  const [scale, setScale] = useState(1); // Initialize scale to 1 instead of 0
 
-  const text = document.getElementById("moon");
+  useEffect(() => {
+    window.addEventListener("scroll", scrollfunction);
+    return () => {
+      window.removeEventListener("scroll", scrollfunction);
+    };
+  }, []);
 
-  window.addEventListener("scroll", () => {
+  const scrollfunction = () => {
+    if (scroll < 300) {
+      let val = window.scrollY;
+    let top = val * 1.2;
+    let finaltop = Math.floor(top);
+      setScroll(finaltop);
+    }
+    else{
     let val = window.scrollY;
-    let top = val * 2 + "px";
-    setScroll(top);
+    let top = val * 1.2;
+    let finaltop = Math.floor(top);
+    setScroll(finaltop);
+  }
+  };
 
-  });
-console.log(scroll)
-  // let val = window.scrollY;
-  // let scroll = val * 2 +'px';
-
-  // console.log(scroll);
+  console.log({ scroll });
+  console.log({ scale });
 
   return (
     <>
-      <div className="bg-black h-[700px] ">
-        <div className={`flex left-[500px] absolute top-[${scroll}]`}>
-          <img className=" w-[100px]" src="moon.png" alt="" />
-        </div>
+      <div className="bg-black relative h-[180vh] overflow-hidden">
+        <img
+          style={{ bottom: scroll, transform: `scale(${scale})` }}
+          className="w-[130px] h-[100px] absolute left-[500px]"
+          src="moon.png"
+          alt=""
+        />
       </div>
     </>
   );
