73cf342fd1c82a0b89a2427414181ebe2df1c5ab
Syed Muhammad Muhtasham <muhtasham00@gmail.com>
2024-08-15 13:57:39 +0500

    useRef hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from 'react';
+import './input.css';
 
 const App = () => {
-  
-  const Vid = useRef(null);
+  const [scroll, setScroll] = useState(0);
 
-  const playVid = () => {
-    if (Vid.current) {
-      Vid.current.play();
-    }
-  };
-  const pauseVid = () => {
-    if (Vid.current) {
-      Vid.current.pause();
-    }
-  };
-  const ahead = (time) => {
-    if (Vid.current) {
-      Vid.current.currentTime = time;
-    }
-  };
+  const boxRefs = useRef(null);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setScroll(window.scrollY);
+    };
 
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); 
 
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+console.log(scroll)
   return (
     <>
-      <div className="w-[100vh] m-auto">
-        <video className="w-[600px]" ref={Vid} src="video.mp4"></video>
+      <div className='box w-[100%] h-[100vh] py-10 text-center border-2 border-black bg-slate-400'>
+        Box 1
       </div>
-      <div className="flex justify-center gap-2">
-        <button onClick={playVid} className="bg-slate-400 mt-2 px-4 py-1 rounded-lg">
-          Play
-        </button>
-        <button onClick={pauseVid} className="bg-slate-400 mt-2 px-4 py-1 rounded-lg">
-          Pause
-        </button>
-        <button onClick={() => ahead(10)} className="bg-slate-400 mt-2 px-4 py-1 rounded-lg">
-          10+
-        </button>
+      <div className='box w-[100%] h-[100vh] py-10 text-center border-2 border-black bg-emerald-400'>
+        Box 2
+      </div>
+      <div className='box w-[100%] h-[100vh] py-10 text-center border-2 border-black bg-lime-400'>
+        Box 3
+      </div>
+      <div
+        ref={boxRefs}
+        className={`box h-[100vh] w-[100%] py-10 text-center border-2 border-black bg-amber-400 ${scroll>1900 ? 'in-view' : ''}`}
+      >
+        Box 4
       </div>
     </>
   );
