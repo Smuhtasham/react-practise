b21931c075e32a488ddf420c50f35e482d1e42b0
Syed Muhammad Muhtasham <muhtasham00@gmail.com>
2024-08-17 13:55:36 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Login from "./components/Login";
 import { Route, Routes } from "react-router-dom";
 import Signup from "./components/Signup";
+import Main from "./components/Main";
 
 const App = () => {
   return (
@@ -9,7 +10,8 @@ const App = () => {
       <div className="bg-white h-[100vh]">
         <div className="flex flex-col items-center justify-center py-10">
           <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<Main />} />
+            <Route path="/Login" element={<Login />} />
             <Route path="/Signup" element={<Signup />} />
           </Routes>
         </div>
