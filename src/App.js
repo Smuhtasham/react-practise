1aa2643b16a7c20f3c2a1172e2e7040bbbad941a
Syed Muhammad Muhtasham <muhtasham00@gmail.com>
2024-08-22 20:05:18 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import { useGetAllPostQuery } from './Services/Post'
+import React from "react";
+import { useGetAllPostQuery } from "./Services/Post";
 
 const App = () => {
-
-const apiData = useGetAllPostQuery(50);
-console.log(apiData)
-if(apiData.isLoading) return <div>Loading....</div>
-if(apiData.isError) return <div>Error Occured......</div>
+  const apiData = useGetAllPostQuery(50);
+  console.log(apiData);
+  if (apiData.isLoading) return <div>Loading....</div>;
+  if (apiData.isError) return <div>Error Occured......</div>;
   return (
     <>
-    {apiData.data.results.map((data)=>
-    <div>{data.name.first}</div>
-    )}
+      {apiData.data.results.map((data) => (
+        <div>{data.name.first}</div>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Services/Post.js
+++ src/Services/Post.js
@@ -1,18 +1,19 @@
 import { createApi, fet, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const postApi= createApi({
-reducerPath:'postApi',
-baseQuery: fetchBaseQuery({
-    baseUrl:"https://randomuser.me/api/",
-}),
-endpoints:(builder)=>({
+export const postApi = createApi({
+  reducerPath: "postApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://randomuser.me/api/",
+  }),
+  endpoints: (builder) => ({
     getAllPost: builder.query({
-        query:(number)=>({
-            url:`?results=${number}`,
-            method: 'GET'
-        })
-    })
-})
-})
+      query: (number) => ({
+        url: `?results=${number}`,
+        method: "GET",
+      }),
+      
+    }),
+  }),
+});
 
 export const { useGetAllPostQuery } = postApi;
