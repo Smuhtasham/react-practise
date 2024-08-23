83e8b7f6a42e95a07987bba44f4ea1386414cd64
Syed Muhammad Muhtasham <muhtasham00@gmail.com>
2024-08-23 18:39:22 +0500

    Api table with antd & Pagination

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
-import React from "react";
-import { Table } from "antd";
-import { useGetAllPostQuery } from "./Services/Post";
+import React, { useEffect, useState } from "react";
+import { Table, Pagination } from "antd";
 
 const App = () => {
-  const apiData = useGetAllPostQuery();
-  console.log(apiData);
+  const [dataSource, setDataSource] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
-  if (apiData.isLoading) return <div>Loading....</div>;
-  if (apiData.isError) return <div>Error Occured......</div>;
+  useEffect(() => {
+    apiData(1);
+  }, []);
+
+  const apiData = async (page) => {
+    setLoading(true);
+    try {
+      const res = await fetch(
+        `https://randomuser.me/api/?page=${page}&results=100&seed=abc`
+      );
+      const data = await res.json();
+      setDataSource(data);
+      setCurrentPage(data.info.page);
+      setTotalPages(data.info.results);
+      
+    } finally {
+      setLoading(false);
+    }
+  };
+  console.log(dataSource);
+
 
   const columns = [
     {
-      title: 'Email',
-      dataIndex: 'email',
-      key: 'email',
+      title: "Email",
+      dataIndex: "email",
     },
     {
-      title: 'Cell',
-      dataIndex: 'cell',
-      key: 'cell',
+      title: "Gender",
+      dataIndex: "gender",
     },
     {
-      title: 'Gender',
-      dataIndex: 'gender',
-      key: 'gender',
+      title: "Phone",
+      dataIndex: "phone",
     },
   ];
   return (
     <>
-      <div className="w-[90%] m-auto">
-      <Table dataSource={apiData.data.results} columns={columns} />;
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Table
+          loading={loading}
+          columns={columns}
+          dataSource={dataSource.results}
+          pagination={{
+            pageSize: 10,
+            current:currentPage,
+            total: totalPages,
+            onChange: (page) => {
+              apiData(page);
+            },
+          }}
+        ></Table>
       </div>
     </>
   );
--- src/Services/Post.js
+++ src/Services/Post.js
@@ -1,9 +1,9 @@
-import { createApi, fet, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const postApi = createApi({
   reducerPath: "postApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://randomuser.me/api/?page=3&results=50&seed=abc",
+    baseUrl: "https://randomuser.me/api/",
   }),
   endpoints: (builder) => ({
     getAllPost: builder.query({
